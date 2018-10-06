fbcc07d472a15b3f0e0314cf06eefe9d7bfc37ae
Dushyant Singh <dushyant.iiit@gmail.com>
2018-10-06 16:07:22 +0530

    Merge fixed

--- src/lang/en.js
+++ src/lang/en.js
@@ -1,6 +1,5 @@
 // English
 const lang = {
-<<<<<<< HEAD
   contact: 'Contact',
   born: 'Born',
   bornIn: 'in',
@@ -13,16 +12,5 @@ const lang = {
   achievements: 'Achievements',
   other: 'Others',
   responsibility: 'Responsibility'
-=======
-    contact: 'Contact',
-    born: 'Born',
-    bornIn: 'in',
-    experience: 'Experience',
-    education: 'Education',
-    skills: 'Skills',
-    projects: 'Projects',
-    contributions: 'Contributions',
-    about: 'About me'
->>>>>>> 9d7fcd708a553916758fd69fbd4ebe6e32e134e5
 };
 export default lang;
